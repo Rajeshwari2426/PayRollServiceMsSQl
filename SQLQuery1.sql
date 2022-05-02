74afd71d06ca69de176dc9c1a9bf84fd7553e8da
Rajeshwari2426 <rajarajeshwaribrgandi@gmail.com>
2022-05-02 12:40:22 +0530

    [raji]adding data to table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -7,4 +7,5 @@ CREATE TABLE EmployeePayRoll(
 	Name varchar(200),
 	Salary float,
 	startDate date);
-  
+-------UC3-Insert data to table-----
+INSERT INTO EmployeePayRoll (Name,Salary,startDate) Values('Rajeshwari',70000,getdate());
