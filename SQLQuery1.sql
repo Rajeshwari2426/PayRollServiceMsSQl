fe05e32714cd2e2ff593197cbe20877801f22f5c
Rajeshwari2426 <rajarajeshwaribrgandi@gmail.com>
2022-05-02 12:24:11 +0530

    [raji] creating table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,10 @@
 ----UC1 create database----
 create database PayRollService;
 use PayRollService;
+-----UC2-Create Table----
+CREATE TABLE EmployeePayRoll(
+	Id int identity(1,1) primary key,
+	Name varchar(200),
+	Salary float,
+	startDate date);
+  
