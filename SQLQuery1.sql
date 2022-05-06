ec7b99503f192224b6ea14896e3d8ef699ae98ea
Rajeshwari2426 <rajarajeshwaribrgandi@gmail.com>
2022-05-06 20:45:34 +0530

    [Rajeshwari]adding PayRollService Table Extentiion

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -26,3 +26,34 @@ select * from EmployeePayRoll where StartDate between cast ('2018-01-01' as date
 alter table EmployeePayRoll add Gender char(1);
 update EmployeePayRoll set Gender = 'M' where Id in (2,3,4,5);
 update EmployeePayRoll set Gender = 'F' where Id in (1,6,7);
+--UC7-- find sum, average, min, max and number of male and female employees
+select sum(Salary) as sumsalary,Gender from EmployeePayRoll group by Gender;
+select avg(Salary) as avgsalary,Gender from EmployeePayRoll group by Gender; 
+select max(Salary) as maxsalary,Gender from EmployeePayRoll group by Gender; 
+select min(Salary) as minsalary,Gender from EmployeePayRoll group by Gender; 
+select count(Name) as EmployeeCount,Gender from EmployeePayRoll group by Gender;  
+--UC8-- add employee phone, department(not null), Address (using default values)
+select * from EmployeePayRoll;
+alter table EmployeePayRoll add Phone bigint;
+update EmployeePayRoll set Phone = 9034569874 where Id IN (5,4,6,7);
+update EmployeePayRoll set Phone = 9505569874 where Id IN (1,2,3);
+alter table EmployeePayRoll add Address varchar(100) not null default 'Hyderabad';
+alter table EmployeePayRoll add Department varchar(250) not null default 'IT';
+--UC9-- Extend table to have Basic Pay, Deductions, Taxable Pay, Income Tax, Net Pay.
+alter table EmployeePayRoll add Basic_Pay float not null default 0.00;
+update EmployeePayRoll set Basic_Pay=70000.00 where Id in (1,6,7); 
+update EmployeePayRoll set Basic_Pay=50000.00 where Id=2;
+update EmployeePayRoll set Basic_Pay=60000.00 where Id=3;
+update EmployeePayRoll set Basic_Pay=40000.00 where Id=4;
+update EmployeePayRoll set Basic_Pay=50000.00 where Id=5;
+alter table EmployeePayRoll add Deductions float not null default 0.00,
+								 Taxable_Pay float not null default 0.00, 
+								 Income_Tax float not null default 0.00,
+								 Net_Pay float not null default 0.00;
+select * from EmployeePayRoll;
+update EmployeePayRoll set Net_Pay = (Basic_Pay-Deductions-Taxable_Pay-Income_Tax);
+--UC10-- Two departments for same employee
+insert into EmployeePayRoll (Name, Salary, startDate, Gender, Phone, Address, Department, Basic_Pay, Deductions, Taxable_Pay, Income_Tax) values ('Dhoni', 60000.00, '2011-07-07', 'M', 950561230, 'Chennai', 'Sports', 60000.00, 1000.00, 2000.00, 1500.00);
+								 
+
+
